96b0ae61d84dc23ded5509a1df70ea18372c478b
Manuel Pineda Varela <159482318+ING69COOLER@users.noreply.github.com>
2025-09-26 07:47:18 -0500

    first commit

---
+++ src/main/java/co/proyecto/proyecto_estrucutra_datos/ProyectoEstrucutraDatosApplication.java
@@ -0,0 +1,13 @@
+package co.proyecto.proyecto_estrucutra_datos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEstrucutraDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEstrucutraDatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/proyecto/proyecto_estrucutra_datos/ProyectoEstrucutraDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.proyecto.proyecto_estrucutra_datos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEstrucutraDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
