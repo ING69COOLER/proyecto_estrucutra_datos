a134b1002fa6b9fe32b0a59668602cc95a36aac3
Manuel Pineda Varela <159482318+ING69COOLER@users.noreply.github.com>
2025-10-22 16:14:30 -0500

    feat: se arreglo la estrucuta del proyecto  se mejoro el UML

--- backend/src/main/java/co/proyecto/proyecto_estrucutra_datos/ProyectoEstrucutraDatosApplication.java
+++ backend/src/main/java/co/proyecto/proyecto_estrucutra_datos/ProyectoEstrucutraDatosApplication.java
@@ -2,6 +2,8 @@ package co.proyecto.proyecto_estrucutra_datos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ProyectoEstrucutraDatosApplication {
@@ -10,4 +12,12 @@ public class ProyectoEstrucutraDatosApplication {
 		SpringApplication.run(ProyectoEstrucutraDatosApplication.class, args);
 	}
 
+}
+
+@Controller
+class InicioController {
+	@GetMapping("/")
+	public String inicio() {
+		return "inicio";
+	}
 }
